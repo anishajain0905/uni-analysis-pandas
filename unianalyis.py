06e8e544f20a452065bfd5dfbaf54e24d5463c94
Anisha Jain <anishajain0905@gmail.com>
2026-01-07 02:59:56 +0700

    [feat] summary of top 10 unis in france and netherlands

--- unianalyis.py
+++ unianalyis.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1400)
+pd.set_option('display.colheader_justify', 'left')
+
 '''#loading the csv 2024 from world university rankings
 df = pd.read_csv('2024_rankings.csv')
 print("Columns in the dataset:", df.columns)
@@ -18,11 +22,29 @@ df= pd.read_csv('qs-world-rankings-2025.csv')
 print("columns:", df.columns)
 df= df[['Institution Name','2025 Rank', '2024 Rank','Location Full', 'Academic Reputation', 'International Students', 'Employment Outcomes', 'QS Overall Score']]
 
+print(df.head(10))
+
+# Extract numeric part of the '2025 Rank' for sorting
+df['Rank_numeric'] = (
+    df['2025 Rank']
+    .astype(str)
+    .str.extract(r'^(\d+)')   # take the first number only
+    .astype(float)
+)
+# Sort by the numeric rank and get top 10 universities in france
+france_unis = df[df['Location Full'] == "France"]
+france_unis = france_unis.sort_values(by='Rank_numeric', ascending=True).head(10)
+
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 1000)
 
-print(df.head(10))
-
-france_unis = df[df['Location Full'] == "France"]
-france_unis = france_unis.sort_values(by='2025 Rank')
+print("Top 10 universities in France in 2025:")
 print(france_unis[['Institution Name', '2025 Rank', 'QS Overall Score']])
+
+netherlands_unis = (
+    df[df['Location Full'] == 'Netherlands']
+    .sort_values('Rank_numeric', ascending=True)
+)
+
+print("\nTop 10 universities in the Netherlands in 2025:")
+print(netherlands_unis[['Institution Name', '2025 Rank', 'QS Overall Score']].head(10))
